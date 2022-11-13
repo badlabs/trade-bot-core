40734b83dc936f5497983c1267ecfaa3ec3e156f
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-13 18:29:04 +0400

    Move utils from modules

--- example/exchange-client/TradeModule.ts
+++ example/exchange-client/TradeModule.ts
@@ -1,78 +1,38 @@
 import { CreateOrderOptions } from '../../lib/types'
 import {AbstractTradeModule} from '../../lib/abstract'
-import {
-  PlacedLimitOrder,
-  PlacedMarketOrder
-} from '@tinkoff/invest-openapi-js-sdk'
 import {Domain} from "../Domain";
 import {GetOrderType} from "../../lib/types/extractors";
 import {ExchangeClient} from "./ExchangeClient";
+import {getFigi, placedLimitOrderToOrder, placedMarketOrderToOrder} from "../utils";
 
 export class TradeModule extends AbstractTradeModule<ExchangeClient>{
 
-  private static placedLimitOrderToOrder(order: PlacedLimitOrder, figi: string, price: number): GetOrderType<Domain> {
-    return {
-      figi,
-      operation: order.operation,
-      price,
-      status: order.status,
-      orderId: order.orderId,
-      requestedLots: order.requestedLots,
-      type: "Limit",
-      executedLots: order.executedLots
-    }
-  }
-
-  private async placedMarketOrderToOrder(order: PlacedMarketOrder, figi: string, ticker: string):
-      Promise<GetOrderType<Domain>> {
-    const price = await this.exchangeClient.infoModule.getSecurityLastPrice(ticker)
-    return {
-      figi,
-      operation: order.operation,
-      price,
-      status: order.status,
-      orderId: order.orderId,
-      requestedLots: order.requestedLots,
-      type: "Market",
-      executedLots: order.executedLots
-    }
-  }
-
-  // TODO: move to utils
-  private async getFigi(ticker: string): Promise<string> {
-    const { exchangeClient } = this
-    const security = await exchangeClient.infoModule.getSecurity(ticker, false)
-    if (!security) throw new Error(`Security ${ticker} not found`)
-    const { figi } = security
-    return figi
-  }
-
   public async sell({ ticker, lots, price }: CreateOrderOptions) {
     const { exchangeClient } = this
-    const figi = await this.getFigi(ticker)
+    const figi = await getFigi(ticker, exchangeClient)
     const placedOrder = await exchangeClient.api.limitOrder({figi, operation: 'Sell', lots, price})
-    return TradeModule.placedLimitOrderToOrder(placedOrder, figi, price)
+    return placedLimitOrderToOrder(placedOrder, figi, price)
   }
 
   public async buy({ ticker, lots, price }: CreateOrderOptions) {
     const { exchangeClient } = this
-    const figi = await this.getFigi(ticker)
+    const figi = await getFigi(ticker, exchangeClient)
     const placedOrder = await exchangeClient.api.limitOrder({figi, operation: 'Buy', lots, price})
-    return TradeModule.placedLimitOrderToOrder(placedOrder, figi, price)
+    return placedLimitOrderToOrder(placedOrder, figi, price)
   }
 
   public async marketSell({ ticker, lots }: CreateOrderOptions) {
     const { exchangeClient } = this
-    const figi = await this.getFigi(ticker)
+    const figi = await getFigi(ticker, exchangeClient)
     const placedOrder = await exchangeClient.api.marketOrder({figi, operation: 'Sell', lots})
-    return this.placedMarketOrderToOrder(placedOrder, figi, ticker)
+    return placedMarketOrderToOrder(placedOrder, figi, ticker, this.exchangeClient)
   }
 
   public async marketBuy({ ticker, lots }: CreateOrderOptions) {
     const { exchangeClient } = this
-    const figi = await this.getFigi(ticker)
+    const figi = await getFigi(ticker, exchangeClient)
     const placedOrder = await exchangeClient.api.marketOrder({figi, operation: 'Buy', lots})
-    return this.placedMarketOrderToOrder(placedOrder, figi, ticker)
+    return placedMarketOrderToOrder(placedOrder, figi, ticker, this.exchangeClient)
   }
 
   public async sellOrCancel(): Promise<GetOrderType<Domain>> {
