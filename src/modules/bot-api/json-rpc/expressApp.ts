3bb1262dcce54d18f03b66b931c8f6c41fe31753
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-26 15:19:24 +0400

    Use JSON RPC to handle api requests

---
+++ src/modules/bot-api/json-rpc/expressApp.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import {createService} from "json-rpc-ts-wrapper";
+import { apiRouter } from './routers'
+import {initServiceMethods} from "./service";
+import {TradeBot} from "../../../TradeBot";
+
+export function initExpress(tradeBot: TradeBot){
+    const app = express()
+    app.use(express.json())
+    app.use(cors())
+
+    const packageJson = require('../../../../package.json')
+
+    app.get('/', function (req, res) {
+        res.send(`This is tradebot v${packageJson.version}`)
+    })
+
+    app.use('/api', apiRouter)
+
+    app.set('tradeBot', tradeBot)
+    const jsonRpcService = createService(initServiceMethods(tradeBot))
+
+    app.post('/json-rpc', (req, res) => {
+        jsonRpcService
+            .receive(req.body)
+            .then(response => response ? res.send(response) : res.sendStatus(204))
+    })
+
+    return app
+}
