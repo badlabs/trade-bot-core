4edfbdfd9d079cd822d8a7d464befb89af200a5d
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-30 00:03:23 +0400

    Logs draft

--- cli/App.tsx
+++ cli/App.tsx
@@ -1,18 +1,43 @@
 import React, {useState} from 'react';
+import SelectInput from 'ink-select-input';
 import Layout from "./Layout";
-import Navigation from "./Navigation";
+import {HomeScreen} from "./screens/HomeScreen";
+import {BotConnectScreen} from "./screens/BotConnectScreen";
+import LogsScreen from "./screens/LogsScreen";
 
 export default () => {
     const [botConfig, setBotConfig] = useState({
-        id: '',
+        id: 'Test',
         host: 'localhost',
         port: 4268,
         token: '',
     })
+    const [screen, setScreen] = useState('')
+
+    let CurrentScreen: JSX.Element = <HomeScreen/>
+    if (!botConfig?.id)
+        CurrentScreen = <BotConnectScreen botConfig={botConfig} />
+    else {
+        switch (screen) {
+            case 'logs':
+                CurrentScreen = <LogsScreen botConfig={botConfig} />
+                break
+            default:
+                function handleSelect(item: { label: string, value: string }){
+                    setScreen(item.value)
+                }
+                CurrentScreen = <SelectInput items={[
+                    { label: 'View Logs', value: 'logs' },
+                    { label: 'Execute Command', value: 'exec' },
+                ]} onSelect={handleSelect} />
+                break
+        }
+    }
+
 
     return (<>
         <Layout>
-            <Navigation botConfig={botConfig} />
+            { CurrentScreen }
         </Layout>
     </>)
 }
--- cli/screens/BotConnectScreen.tsx
+++ cli/screens/BotConnectScreen.tsx
@@ -1,8 +1,15 @@
-import {Text} from "ink";
+import {Newline, Text} from "ink";
 import React from "react";
+import {BotConfig} from "../models";
 
-export const BotConnectScreen = () => {
+export const BotConnectScreen = (props: {
+    botConfig: BotConfig
+}) => {
     return (<>
-        <Text>To connect to bot</Text>
+        <Text>To connect to bot</Text><Newline />
+        <Text>ID: {props.botConfig.id}</Text><Newline />
+        <Text>Host: {props.botConfig.host}</Text><Newline />
+        <Text>Port: {props.botConfig.port}</Text><Newline />
+        <Text>Token: {props.botConfig.token}</Text><Newline />
     </>)
 }
