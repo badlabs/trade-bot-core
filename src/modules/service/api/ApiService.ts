8225922d3fedfde08c6797e199c2a7b18621e581
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-17 21:09:57 +0400

    Colors for tradebot online message

--- src/modules/service/api/ApiService.ts
+++ src/modules/service/api/ApiService.ts
@@ -1,11 +1,12 @@
 import { Express } from 'express'
 import http from 'http'
-import ws, {WebSocketServer} from 'ws';
+import ws, {WebSocketServer} from 'ws'
+import colors from "colors/safe"
 import { TradeBot } from '../../../TradeBot'
 import { initExpress } from './express'
 import { config } from '../../../config'
 import {HandleError} from "../../../utils";
-import {registerExpressRoutes, registerWSSHandler} from "./trpc";
+import {registerExpressRoutes, registerWSSHandler} from "./trpc"
 
 export class ApiService {
   private readonly tradeBot: TradeBot
@@ -34,9 +35,9 @@ export class ApiService {
       tradeBot: this.tradeBot
     })
     this.http.listen(config.api.port, () => {
-      console.info(`[i] TradeBot is online on: `)
-      console.info(`  [i] REST API - http://${config.api.host}:${config.api.port}/`)
-      console.info(`  [i] WebSocket - ws://${config.api.host}:${config.api.port}/`)
+      console.info(`${colors.blue('[i]')} TradeBot is online on: `)
+      console.info(`  ${colors.blue('[i]')} REST API - http://${config.api.host}:${config.api.port}/`)
+      console.info(`  ${colors.blue('[i]')} WebSocket - ws://${config.api.host}:${config.api.port}/`)
     })
   }
 }
